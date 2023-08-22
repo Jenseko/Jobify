079c976228a5719043f4e8ba6b05cd2eca8eb31b
Jenseko <jenscode23@gmail.com>
2023-08-22 23:20:03 +0200

    add some Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import HomeLayout from "./pages/HomeLayout";
+import {
+  HomeLayout,
+  Register,
+  Login,
+  DashboardLayout,
+  Landing,
+  Error,
+} from "./pages";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomeLayout />,
-  },
-  {
-    path: "/about",
-    element: (
-      <div>
-        <h1>about page</h1>
-      </div>
-    ),
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+    ],
   },
 ]);
 
