524a3c3b065b07fef760de132842afa00e148a29
Jenseko <jenscode23@gmail.com>
2023-08-22 22:13:10 +0200

    install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomeLayout from "./pages/HomeLayout";
+HomeLayout;
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>home</h1>,
+  },
+  {
+    path: "/about",
+    element: (
       <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>about page</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
-}
+    ),
+  },
+]);
 
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 export default App;
